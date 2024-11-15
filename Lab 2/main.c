3d296b977942b58a19ca7b76171aa356a8d9e77b
tobiaspersonn <tobbepersson11@gmail.com>
2024-11-15 14:39:28 +0100

    Update main.c

--- Lab 2/main.c
+++ Lab 2/main.c
@@ -3,12 +3,16 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <math.h>
+#include <stdatomic.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #define NTHRD 8
 #define RADIUS 1
 #define SQUARE_AREA (4.0 * RADIUS * RADIUS)
 #define CIRCLE_AREA (M_PI * RADIUS * RADIUS)
 
+// Assignment 1
 double Nhit = 0.0;
 double Ntry = 0.0;
 
@@ -55,9 +59,67 @@ void montecarlo() {
     }
 }
 
+// Assignment 2
+
+int counter = 0;
+const int nthr = 1000;
+
+typedef struct {
+    /* MEMBERS TO BE DEFINED */
+    int value;
+    struct process *list;
+} mysemaphore_t;
+
+mysemaphore_t sem;
+
+int sem_init ( mysemaphore_t *s ) {
+    /* FUNCTION TO BE IMPLEMENTED */
+    atomic_flag_clear(&s->value);
+    ATOMIC_VAR_INIT(s);
+    return 0;
+}
+
+int sem_wait ( mysemaphore_t *s ) {
+    /* FUNCTION TO BE IMPLEMENTED */
+    while(s->value > 0);
+    atomic_flag_test_and_set(&s->value);
+    // if (s->value == 0) {
+    //     sleep();
+    // }
+    return 0;
+}
+
+int sem_post ( mysemaphore_t *s ) {
+    /* FUNCTION TO BE IMPLEMENTED */
+    atomic_flag_clear(&s->value);
+    // if (s->value == 0) {
+    //     wakeup();
+    // }
+    return 0;
+}
+
+void* func() {
+    sleep(1);
+    sem_wait(sem);
+    counter++;
+    sem_post(sem);
+}
+
 int main(int argc, char const *argv[])
 {
     montecarlo();
+
+    pthread_t thr[nthr];
+
+    sem_init(&sem);
+
+    for (int i = 0; i < nthr; i++)
+    pthread_create(&thr[i], NULL, &func , NULL);
+
+    for (int i = 0; i < nthr; i++)
+    pthread_join(thr[i], NULL);
+
+    printf("counter = %d\n", counter);
 
     return 0;
 }
