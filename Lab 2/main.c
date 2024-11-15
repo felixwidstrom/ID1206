3fd84fd9f57a1ce6f049c2bde9c7ab9de23c6632
tobiaspersonn <tobbepersson11@gmail.com>
2024-11-15 14:56:48 +0100

    Update main.c

--- Lab 2/main.c
+++ Lab 2/main.c
@@ -66,43 +66,35 @@ const int nthr = 1000;
 
 typedef struct {
     /* MEMBERS TO BE DEFINED */
-    int value;
+    atomic_flag value;
     struct process *list;
 } mysemaphore_t;
 
 mysemaphore_t sem;
 
-int sem_init ( mysemaphore_t *s ) {
+int sem_init (mysemaphore_t *s) {
     /* FUNCTION TO BE IMPLEMENTED */
-    atomic_flag_clear(&s->value);
     ATOMIC_VAR_INIT(s);
     return 0;
 }
 
-int sem_wait ( mysemaphore_t *s ) {
+int sem_wait (mysemaphore_t *s) {
     /* FUNCTION TO BE IMPLEMENTED */
-    while(s->value > 0);
-    atomic_flag_test_and_set(&s->value);
-    // if (s->value == 0) {
-    //     sleep();
-    // }
+    while(atomic_flag_test_and_set(&s->value));
     return 0;
 }
 
-int sem_post ( mysemaphore_t *s ) {
+int sem_post (mysemaphore_t *s) {
     /* FUNCTION TO BE IMPLEMENTED */
     atomic_flag_clear(&s->value);
-    // if (s->value == 0) {
-    //     wakeup();
-    // }
     return 0;
 }
 
 void* func() {
     sleep(1);
-    sem_wait(sem);
+    sem_wait(&sem);
     counter++;
-    sem_post(sem);
+    sem_post(&sem);
 }
 
 int main(int argc, char const *argv[])
