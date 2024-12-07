b6f76c634ff51d46f0720fc919a7c00109e8a945
tobiaspersonn <tobbepersson11@gmail.com>
2024-12-07 02:21:12 +0100

    La till uppgift 2

--- Lab 4/main.c
+++ Lab 4/main.c
@@ -2,11 +2,14 @@
 #include <stdlib.h>
 #include <dirent.h>
 #include <sys/types.h>
+#include <sys/time.h>
 #include <string.h>
 
 DIR *dir;
 struct dirent *dp;
 struct dirent **entries = NULL;
+
+#define ARRAY_SIZE (1 << 30) // 1 GiB
 
 void files(char *path)
 {
@@ -47,9 +50,71 @@ void files(char *path)
     }
 }
 
+void write_data(int nio, int nbuf) { // Assignment 2
+    int i;
+
+    char** data = (char**)malloc((ARRAY_SIZE/nio) * sizeof(char*));
+
+    char buffer[nbuf];
+
+    FILE* file = fopen("output", "w");
+    if(file == NULL) {
+        perror("fopen error");
+        exit(1);
+    }
+
+    for(i = 0; i < ARRAY_SIZE/nio; i++){ 
+        data[i] = (char*)malloc(nio);
+        if (data[i] == NULL) {
+            perror("malloc failed");
+            exit(1);
+        }   
+        memset(data[i], '@', nio);
+    }
+
+    if (setvbuf(file, buffer, _IOFBF, sizeof(buffer)) !=0) {
+        perror("Buffer error");
+        exit(1);
+    }
+
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+
+    for (int i = 0; i < ARRAY_SIZE/nio; i++) {
+        fputs(data[i], file);
+    }
+
+    gettimeofday(&end, NULL);
+
+    double time = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1e6;
+    printf("nio = %d, nbuf = %d, I/O time = %.6f seconds\n", nio, nbuf, time);
+
+    fclose(file);
+
+    // Free up the allocated memory
+    for(i = 0; i < ARRAY_SIZE/nio; i++) {
+        free(data[i]);
+    }
+    free(data);
+}
+
 int main(int argc, char const *argv[])
 {
     files("test");
     // buffering();
+
+    // Assignment 2
+    const int nio_min = (1 << 10);
+    const int nio_max = (1 << 15);
+    const int nbuf_min = (1 << 10);
+    const int nbuf_max = (1 << 15);
+
+
+    for(int nio = nio_min; nio <= nio_max; nio <<= 1) {
+        for(int nbuf = nbuf_min; nbuf <= nbuf_max; nbuf <<= 1) {
+            write_data(nio, nbuf);
+        }
+    }
+
     return 0;
 }
